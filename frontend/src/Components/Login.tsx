08c71b2b5c7947305e2233c16aa601e7be3f0236
Sophia Rafat <sophiarafat@sophias-mbp.attlocal.net>
2024-05-02 20:35:07 -0500

    resolve nested git

---
+++ frontend/src/Components/Login.tsx
@@ -0,0 +1,14 @@
+// Login.tsx
+
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const Login: React.FC = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <button onClick={() => loginWithRedirect()}>Log In</button>
+  );
+};
+
+export default Login;
